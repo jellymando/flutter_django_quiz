265a8e884eee7787c70b5b86222dea634510c24b
jellymando <cicikdi@naver.com>
2023-04-30 17:44:19 +0900

    첫 화면 생성

---
+++ lib/screen/screen_home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _State();
+}
+
+class _State extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Quiz App'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(child: Image.asset('assets/INTJ3.jpg', width: width*0.8)),
+            Padding(padding: EdgeInsets.all(width*0.024)),
+            Text('플러터 퀴즈 앱', style: TextStyle(fontSize: width*0.065, fontWeight: FontWeight.bold)),
+            Text('퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.', textAlign: TextAlign.center),
+            Padding(padding: EdgeInsets.all(width*0.048)),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+            Padding(padding: EdgeInsets.all(width*0.048)),
+            Container(
+              padding: EdgeInsets.only(bottom: width*0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width*0.8,
+                  height: height*0.05,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: ElevatedButton(
+                      child: Text('지금 퀴즈 풀기', style: TextStyle(color: Colors.white)),
+                      onPressed: () {},
+                  )
+                )
+              )
+            )
+          ]
+        )
+      )
+    );
+  }
+}
+
+Widget _buildStep(double width, String title) {
+  return Container(
+    padding: EdgeInsets.fromLTRB(width*0.048, width*0.024, width*0.048, width*0.024),
+    child: Row(crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Icon(Icons.check_box,size: width*0.04),
+      Padding(padding:EdgeInsets.only(right: width*0.024)),
+      Text(title)
+    ]),
+  );
+}
+
+
+
